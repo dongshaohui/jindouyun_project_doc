a2c3c86c857e4983ff928398ff26109d2dfb6f5b
zwu2 <718380379@qq.com>
2016-11-22 10:23:22 +0800

    获取所有店铺的信息接口修改

--- ios_docfile/doc.js
+++ ios_docfile/doc.js
@@ -643,10 +643,10 @@ public interface ApiDoc {
 
 	/**
 	 * 
-	 * @api {get} /user_serv/get_all_shop_infos 获取所有店铺的信息
+	 * @api {get} /user_serv/get_all_shop_infos_v2 获取所有店铺的信息
 	 * @apiName 获取所有正在营业中店铺的信息11
 	 * @apiGroup Shop_Module
-	 * @apiVersion 0.1.0
+	 * @apiVersion 0.1.1
 	 * @apiDescription 接口详细描述
 	 * 
 	 * 
